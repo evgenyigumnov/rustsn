0c2744a7ff468daf7745e18b494ee387e05d84e1
Evgeny Igumnov <igumnovnsk@gmail.com>
2024-10-12 18:45:57 +0500

    clean code

--- src/javascript.rs
+++
@@ -1,7 +0,0 @@
-mod test {
-
-    #[test]
-    fn test_parse_llm_response() {
-        todo!()
-    }
-}
--- src/llm_response.rs
+++ src/llm_response.rs
@@ -453,3 +453,41 @@ impl LLMResponse {
         }
     }
 }
+
+
+mod tests {
+    #[test]
+    fn test_parse_llm_response_java() {
+        for i in 1..=5 {
+            let file = format!("./test_data/java_create_{}.txt", i);
+            let response = std::fs::read_to_string(file).unwrap();
+            let project =
+                crate::llm_response::LLMResponse::parse_llm_response(&response, crate::Lang::Java);
+
+            println!("{:#?}", project);
+            assert!(!project.dependencies.is_empty());
+            assert!(!project.solution_code.is_empty());
+            assert!(!project.test_code.is_empty());
+            assert!(!project.build_command.is_empty());
+            assert!(!project.test_command.is_empty());
+        }
+    }
+
+    #[test]
+    fn test_parse_llm_response_rust() {
+        for i in 1..=9 {
+            let file = format!("./test_data/rust_create_{}.txt", i);
+            let response = std::fs::read_to_string(file).unwrap();
+            let mut project =
+                crate::llm_response::LLMResponse::parse_llm_response(&response, crate::Lang::Rust);
+            project.build_command = crate::utils::remove_comments(&project.build_command);
+            project.test_command = crate::utils::remove_comments(&project.test_command);
+
+            println!("{:#?}", project);
+            assert!(!project.dependencies.is_empty());
+            assert!(!project.solution_code.is_empty());
+            assert!(!project.build_command.is_empty());
+            assert!(!project.test_command.is_empty());
+        }
+    }
+}
